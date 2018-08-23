79b2443f51b009373b8cf7c66b14e495e0aa2728
Chase Weyer <cpwfe87@gmail.com>
2018-08-23 02:24:38 -0400

    Added additional dependencies

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 
 // Serve static files for application
-app.user(express.static(__dirname + '/dist'));
+app.user(express.static(__dirname + '/dist/career-site'));
 
 // Will listen on port 8080
 app.listen(process.env.PORT || 8080);
 
 // Allow Angular to handle routing instead of server
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/career-site/index.html'));
 })
